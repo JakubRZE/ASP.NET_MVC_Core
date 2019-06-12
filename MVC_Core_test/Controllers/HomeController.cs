e0bfab23aef6ab34302718f88ec7fc087cdc6179
JakubRZE <jakub.rzm@gmail.com>
2019-06-13 01:48:42 +0200

    HomeController added

---
+++ MVC_Core_test/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Core_test.Models;
+
+namespace MVC_Core_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            return View(pies);
+        }
+    }
+}
