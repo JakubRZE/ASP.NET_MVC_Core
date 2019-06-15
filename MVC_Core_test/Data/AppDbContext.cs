9ed05a18f37a39a338e80a26da58a471fa374699
JakubRZE <jakub.rzm@gmail.com>
2019-06-16 00:43:26 +0200

    AppDbContext and Migrations added

---
+++ MVC_Core_test/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Core_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Core_test.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
--- MVC_Core_test/Startup.cs
+++ MVC_Core_test/Startup.cs
@@ -5,36 +5,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MVC_Core_test.Data;
 using MVC_Core_test.Models;
 
 namespace MVC_Core_test
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            //if some is asking for an IPieRepository, a new MockPieRepository will be returned. (Transient = new instance every time)
-            services.AddTransient<IPieRepository, MockPieRepository>();
+        //if some is asking for an IPieRepository, a new MockPieRepository will be returned. (Transient = new instance every time)
+        services.AddTransient<IPieRepository, PieRepository>();
 
 
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-        }
+        services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStatusCodePages();
+        app.UseStaticFiles();
+        app.UseMvcWithDefaultRoute();
+    }
+}
 }
