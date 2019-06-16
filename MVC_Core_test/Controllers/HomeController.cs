1f128c98dc31d8c3f0a487ed51ba6c0e11be3bfc
JakubRZE <jakub.rzm@gmail.com>
2019-06-16 14:45:51 +0200

    ViewImports, TagHelpers added

--- MVC_Core_test/Controllers/HomeController.cs
+++ MVC_Core_test/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace MVC_Core_test.Controllers
 
             return View(homeViewModel);
         }
+
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
     }
 }
